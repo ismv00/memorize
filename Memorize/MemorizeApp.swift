25c19021668529b14bdb23c2af659b93d3c9fa14
Igor Menezes <ismv00@icloud.com>
2022-05-17 00:19:48 -0400

    first-commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Igor S. Menezes on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
